27749c6e9fcdeacec7eccf66d8d2d6f19991a760
Максим Радченко <tagr2101@yandex.ru>
2022-08-15 17:47:54 +0300

    Рабочая версия перед финалом

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -3,6 +3,13 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.TextField()
+    slug = models.SlugField()
+    description = models.TextField()
+    
+    def __str__(self):
+        return self.title
 
 class Post(models.Model):
     text = models.TextField()
@@ -11,4 +18,11 @@ class Post(models.Model):
         User,
         on_delete=models.CASCADE,
         related_name='posts'
+    ) 
+    group = models.ForeignKey(
+        Group,
+        on_delete=models.CASCADE,
+        related_name='posts',
+        blank=True, 
+        null=True
     ) 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from django.http import HttpResponse
 
-from .models import Post
+from .models import Post, Group
 
 # Главная страница
 def index(request): 
@@ -19,7 +19,21 @@ def index(request):
 # Страница с постами, отфильтрованными по группам;
 # view-функция принимает параметр slug из path()
 def group_posts(request, slug):
-    return HttpResponse(f'Посты по группам {slug}') 
+    # Функция get_object_or_404 получает по заданным критериям объект 
+    # из базы данных или возвращает сообщение об ошибке, если объект не найден.
+    # В нашем случае в переменную group будут переданы объекты модели Group,
+    # поле slug у которых соответствует значению slug в запросе
+    group = get_object_or_404(Group, slug=slug)
+
+    # Метод .filter позволяет ограничить поиск по критериям.
+    # Это аналог добавления
+    # условия WHERE group_id = {group_id}
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
+    context = {
+        'group': group,
+        'posts': posts,
+    }
+    return render(request, 'posts/group_list.html', context)
 
 def group_list(request):
     template = 'posts/group_list.html' 
